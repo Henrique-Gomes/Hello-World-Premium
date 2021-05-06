5f05c28314955cefe4778e9a769b8d8b00e2c9f4
Luiz <eldorado.luiz@gmail.com>
2021-05-06 11:42:29 -0300

    * Removed console when double clicking hwp_gui on Windows * Now it works on Linux

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -1,6 +1,17 @@
-#include <wx/wx.h>
+#include <wx/wxprec.h>
+#ifndef WX_PRECOMP
+    #include <wx/wx.h>
+#endif
 
-int main() {
+class HWPApp : public wxApp {
+public:
+	virtual bool OnInit();
+};
+
+// This add the 'main' to the program
+wxIMPLEMENT_APP(HWPApp);
+
+bool HWPApp::OnInit() {
 	wxMessageBox("Hello, world!", "Hello World Premium", wxOK | wxCENTRE);
-	return 0;
+	return false;
 }
